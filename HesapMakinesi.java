585cb1f3afc9908be9b0eba025f1361d1d062543
magique <kadirbey832832@gmail.com>
2022-11-20 12:34:35 +0300

    Create HesapMakinesi.java

---
+++ HesapMakinesi.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class HesapMakinesi {
+    public static void main(String[] args) {
+        int n1, n2, select;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type the first number: ");
+        n1 = scan.nextInt();
+        System.out.println("Type the second number: ");
+        n2 = scan.nextInt();
+
+        System.out.println("1-Addition\n2-Subtraction\n3-Multiplication\n4-Division");
+        System.out.println("Your selection: ");
+        select = scan.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("Addition: " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Subtraction: " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Multiplication: " + (n1 * n2));
+                break;
+            case 4:
+                if(n2 != 0)
+                    System.out.println("Division: " + ((float)n1 / (float)(n2)));
+                else
+                    System.out.println("A number can't be divided by zero!");
+                break;
+        }
+    }
+}
+
